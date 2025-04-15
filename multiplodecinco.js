48ef1a6ca957de2ebaac86f8e945f4c01744c4d8
unknown <miguelsellaprado10@gmail.com>
2025-04-15 10:07:55 -0300

    Primeiro Commit

--- multiplodecinco.js
+++ multiplodecinco.js
@@ -4,11 +4,11 @@ if(isNaN(multiplo)){
     alert("Esse número não é valido")
 
 }else{
-    if(multiplo % 5 == 0);
-    alert
+    if(multiplo % 5 == 0){
+    alert ("Este número é múltiplo de 5!")
 
-}else{
-    alert
+} else {
+    alert ("Este número não é múltiplo de 5!")
 
 }
-
+}
